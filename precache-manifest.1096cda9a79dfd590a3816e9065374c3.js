192c43c37689366a1b9209677e35160616776526
fawkes <fawkes@pop-os.localdomain>
2019-06-14 00:46:48 -0300

    Updates

---
+++ precache-manifest.1096cda9a79dfd590a3816e9065374c3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a68c8b541cde6d128858b8be3b33726",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c84959645fa0d74f4cfb",
+    "url": "/static/css/main.ca8ff710.chunk.css"
+  },
+  {
+    "revision": "a5d518f4d9c8ce0e6ae3",
+    "url": "/static/js/2.eea2d298.chunk.js"
+  },
+  {
+    "revision": "c84959645fa0d74f4cfb",
+    "url": "/static/js/main.00736916.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "787c6af98ac770e43e65815a8ff337df",
+    "url": "/static/media/camera.787c6af9.svg"
+  },
+  {
+    "revision": "e2a499354e3b1d309ff71584d7290d29",
+    "url": "/static/media/comment.e2a49935.svg"
+  },
+  {
+    "revision": "00e13584091199bddd3785adaf0b9f3d",
+    "url": "/static/media/like.00e13584.svg"
+  },
+  {
+    "revision": "83fd0bcc6cdc21e671ae5edf856ba3ef",
+    "url": "/static/media/logo.83fd0bcc.svg"
+  },
+  {
+    "revision": "22d960a9ac06cc252579791795bdea8c",
+    "url": "/static/media/more.22d960a9.svg"
+  },
+  {
+    "revision": "55e74cfdc1470a74c5bc8c2af6ed9fa0",
+    "url": "/static/media/send.55e74cfd.svg"
+  }
+]);
